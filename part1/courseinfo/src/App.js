1fdc40a03a4f71abba4bddc00ee394668caa7980
Felipe Huerta Aros <hueeerta@gmail.com>
2022-10-02 02:06:13 -0300

    Part 1.3: Course information, step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,22 +1,37 @@
-import Header from './Header.js'
-import Content from './Content.js'
-import Total from './Total.js'
+import Header from "./Header.js";
+import Content from "./Content.js";
+import Total from "./Total.js";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  return(
+  return (
     <div className="app">
       <Header title={course} />
-      <Content part1name={part1} part2name={part2} part3name={part3} part1exercises={exercises1} part2exercises={exercises2} part3exercises={exercises3} />
-      <Total totalExercises={exercises1+exercises2+exercises3} />
+      <Content
+        part1name={part1.name}
+        part2name={part2.name}
+        part3name={part3.name}
+        part1exercises={part1.exercises}
+        part2exercises={part2.exercises}
+        part3exercises={part3.exercises}
+      />
+      <Total
+        totalExercises={part1.exercises + part2.exercises + part3.exercises}
+      />
     </div>
-  )
-}
+  );
+};
 export default App;
