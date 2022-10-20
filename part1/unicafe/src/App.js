a0c53090d660f4287a1ca4df1e58a3480daef858
Felipe Huerta Aros <hueeerta@gmail.com>
2022-10-20 01:32:00 -0300

    Part 1.9: Unicafe, step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
+
+  if( good > 0 || neutral > 0 || bad > 0) {
   return (
     <>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
     <p>all {good + neutral + bad}</p>
     <p>average {(good - bad)/(good + neutral + bad)}</p>
     <p>positive {(good/(good + neutral + bad))*100} %</p>
     </>
   )
+  } else {
+    return ( <p>No feedback given</p> )
+  }
 }
 
 const App = () => {
@@ -23,9 +31,6 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
