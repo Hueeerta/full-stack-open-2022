6a39e12c5e0f9e923d68cdd366b6fc830a362cb7
Felipe Huerta Aros <hueeerta@gmail.com>
2022-10-25 08:49:18 -0300

    Part 2.9: The Phonebook, step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState({ name: "", number: "" });
-  const [newName, setNewName] = useState([]);
+  const [newContactList, setNewContactList] = useState([]);
+  const [nameSearch, setNewSearch] = useState("");
+
+  const getDataBase = () => {
+    const contactList = JSON.parse(localStorage.getItem("contactList"));
+    if (contactList) {
+      return contactList;
+    } else {
+      return [];
+    }
+  };
+
+  const postDataBase = (newData) => {
+    localStorage.setItem("contactList", JSON.stringify(newData));
+  };
+
+  useEffect(() => {
+    localStorage.setItem(
+      "contactList",
+      JSON.stringify([
+        { name: "Arto Hellas", number: "040-123456" },
+        { name: "Ada Lovelace", number: "39-44-5323523" },
+        { name: "Dan Abramov", number: "12-43-234345" },
+        { name: "Mary Poppendieck", number: "39-23-6423122" },
+      ])
+    );
+    setNewContactList(getDataBase());
+  }, []);
+
+  const handeSearch = (event) => {
+    setNewSearch(event.target.value);
+    if (event.target.value !== "") {
+      const newSearch = getDataBase().filter(
+        (contact) =>
+          contact.name.toLowerCase().indexOf(event.target.value.toLowerCase()) >
+          -1
+      );
+      setNewContactList(newSearch);
+    } else {
+      setNewContactList(getDataBase());
+    }
+  };
 
   const handleInputChange = (event) => {
     const newPerson = { ...persons };
@@ -12,22 +53,24 @@ const App = () => {
     setPersons(newPerson);
   };
 
-  const handleNewNames = (event) => {
+  const handleNewContactList = (event) => {
     event.preventDefault();
-    const newNameKeys = Object.getOwnPropertyNames(newName);
+    setNewContactList(getDataBase());
+    const newContactListKeys = Object.getOwnPropertyNames(newContactList);
     let inputIsNew = true;
-    for (const key of newNameKeys) {
-      if (newName[key].name === persons.name) {
+    for (const key of newContactListKeys) {
+      if (newContactList[key].name === persons.name) {
         inputIsNew = false;
         break;
       }
     }
     if (inputIsNew) {
-      const newPerson = [...newName];
-      newPerson.push(persons);
-      setNewName(newPerson);
+      const newPersonList = [...newContactList];
+      newPersonList.push(persons);
+      postDataBase(newPersonList);
+      setNewContactList(newPersonList);
     } else {
-      alert(`${persons.name} is already added to phonebook`);
+      alert(`${persons.name} is already added to phonebook.`);
     }
     setPersons({ name: "", number: "" });
   };
@@ -35,6 +78,17 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
+      <label>
+        filter shown with
+        <input
+          id="search"
+          type="text"
+          value={nameSearch}
+          placeholder="name search"
+          onChange={handeSearch}
+        />
+      </label>
+      <h2>add a new</h2>
       <form>
         <label htmlFor="name">
           name:
@@ -42,7 +96,7 @@ const App = () => {
             id="name"
             type="text"
             value={persons.name}
-            placeholder="Contact name"
+            placeholder="contact name"
             onChange={handleInputChange}
           />
         </label>
@@ -53,17 +107,17 @@ const App = () => {
             id="number"
             type="text"
             value={persons.number}
-            placeholder="Contact number"
+            placeholder="contact number"
             onChange={handleInputChange}
           />
         </label>
         <br />
-        <button type="submit" onClick={handleNewNames}>
+        <button type="submit" onClick={handleNewContactList}>
           add
         </button>
       </form>
       <h2>Numbers</h2>
-      {newName.map((contact, index) => (
+      {newContactList.map((contact, index) => (
         <p key={index + "-" + contact.name}>
           {contact.name} {contact.number}
         </p>
